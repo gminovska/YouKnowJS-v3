47db55a547a11fc07eff35a234f3e7fc0a79288f
Gordana Minovska <gordanam@fileonq.com>
2017-12-02 17:47:09 -0800

    Test user login actions

--- test/action-creators.test.js
+++ test/action-creators.test.js
@@ -1,15 +1,15 @@
 import * as a from '../src/actions/user';
 
-describe('User action creators', () => {
-  it('USER_SIGNUP_REQUEST action', () => {
+const user = { name: 'Jake', email: 'nope@donthave.com' };
+
+describe('User signup actions', () => {
+  it('USER_SIGNUP_REQUEST', () => {
     expect(a.userSignupRequest()).toEqual({
       type: 'USER_SIGNUP_REQUEST',
     });
   });
 
-  it('USER_SIGNUP_SUCCESS action', () => {
-    const user = { name: 'Jake', email: 'nope@donthave.com' };
-
+  it('USER_SIGNUP_SUCCESS', () => {
     expect(a.userSignupSuccess(user)).toEqual({
       type: 'USER_SIGNUP_SUCCESS',
       payload: expect.objectContaining({
@@ -22,6 +22,27 @@ describe('User action creators', () => {
   it('USER_SIGNUP_FAILURE', () => {
     expect(a.userSignupFailure()).toEqual({
       type: 'USER_SIGNUP_FAILURE',
+    });
+  });
+});
+
+describe('User login actions', () => {
+  it('USER_LOGIN_REQUEST', () => {
+    expect(a.userLoginRequest()).toEqual({
+      type: 'USER_LOGIN_REQUEST',
+    });
+  });
+
+  it('USER_LOGIN_SUCCESS', () => {
+    expect(a.userLoginSuccess(user)).toEqual({
+      type: 'USER_LOGIN_SUCCESS',
+      payload: user,
+    });
+  });
+
+  it('USER_LOGIN_FAILURE', () => {
+    expect(a.userLoginFailure()).toEqual({
+      type: 'USER_LOGIN_FAILURE',
     });
   });
 });
