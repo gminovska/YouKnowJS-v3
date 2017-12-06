99c4d2fd70bd1d26a888347130298cfc3626b69c
Kamyl <kamillcybulski@gmail.com>
2017-12-06 23:45:22 +0100

    Modify tester component

--- src/containers/Tester.jsx
+++ src/containers/Tester.jsx
@@ -1,27 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
 
-import { fetchQuizRequest } from '../actions/current-quiz';
+import { userSignupRequest } from '../actions/user';
 
 class Tester extends React.Component {
+  /**
+   * @constructor
+   */
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      pass: '',
+    };
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handlePassChange = (e) => {
+    this.setState({
+      pass: e.target.value,
+    });
+  }
+
+  send = () => {
+    this.props.signup(this.state.name, this.state.pass);
+  }
+
+  /**
+   * @returns {object} React element
+   */
   render() {
     return (
       <div>
-        <button onClick={this.props.fetch}> CLICK ME</button>
+        <div>{this.props.error}</div>
+        <FormGroup>
+          <ControlLabel>Username</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.pass}
+            onChange={this.handlePassChange}
+          />
+          <Button
+            onClick={this.send}
+          >
+            Send
+          </Button>
+        </FormGroup>
       </div>
     );
   }
 }
 
 Tester.propTypes = {
-  fetch: PropTypes.func.isRequired,
+  signup: PropTypes.func.isRequired,
+  error: PropTypes.string,
 };
 
-const mapStateToProps = () => ({});
+const mapStateToProps = state => ({
+  error: state.errors.message,
+});
 const mapDispatchToProps = dispatch => ({
-  fetch() {
-    dispatch(fetchQuizRequest('wqpofew'));
+  signup(name, pass) {
+    dispatch(userSignupRequest(name, pass));
   },
 });
 
