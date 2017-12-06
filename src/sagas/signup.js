ded7d7d6be6b96a0c675ace754c7aa0dacb235bb
Gordana Minovska <gordanam@fileonq.com>
2017-12-06 14:31:29 -0800

    Create signup user saga

--- src/sagas/signup.js
+++ src/sagas/signup.js
@@ -2,3 +2,25 @@ import { auth } from 'firebase';
 import { put, takeEvery, call } from 'redux-saga/effects';
 
 import { userSignupFailure, userSignupSuccess } from '../actions/user';
+/**
+ * signupUser
+ * @param {object} action USER_SIGNUP_REQUEST
+ * Create a new user
+ * @returns {object} Iterator
+ */
+export function* signupUser(action) {
+  try {
+    const user = yield auth().createUserWithEmailAndPassword(action.email, action.password);
+    yield put(userSignupSuccess(user));
+  } catch (e) {
+    yield put(userSignupFailure(e));
+  }
+}
+
+/**
+ * watch SignupUser
+ * @returns {object} Iterator
+ */
+export default function* watchSignupUser() {
+  yield takeEvery('USER_SIGNUP_REQUEST', signupUser);
+}
