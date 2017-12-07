c63060c43e35c5b3d9089ec53466d42d283b3468
Gordana Minovska <gordanam@fileonq.com>
2017-12-06 16:17:37 -0800

    Create login saga

---
+++ src/sagas/login.js
@@ -0,0 +1,28 @@
+import { auth } from 'firebase';
+import { put, takeEvery, call } from 'redux-saga/effects';
+import { userLoginFailure, userLoginSuccess } from '../actions/user';
+
+/**
+ * loginUser
+ * @param {object} action USER_LOGIN_REQUEST
+ * Create a new user
+ * @returns {object} Iterator
+ */
+export function* loginUser(action) {
+  try {
+    const authUser = auth();
+    const user = yield authUser.signInWithEmailAndPassword(action.email, action.password);
+
+    yield put(userLoginSuccess(user));
+  } catch (e) {
+    yield put(userLoginFailure(e));
+  }
+}
+
+/**
+ * watch loginUser
+ * @returns {object} Iterator
+ */
+export default function* watchLoginUser() {
+  yield takeEvery('USER_LOGIN_REQUEST', loginUser);
+}
--- src/sagas/root.js
+++ src/sagas/root.js
@@ -3,7 +3,7 @@ import { all } from 'redux-saga/effects';
 import watchFetchAllQuizzes from './fetch-all-quizzes';
 import watchFetchQuiz from './fetch-quiz';
 import watchSignupUser from './signup';
-
+import watchLoginUser from './login';
 /**
  * rootSaga
  * @returns {object} Iterator
@@ -13,5 +13,6 @@ export default function* rootSaga() {
     watchFetchAllQuizzes(),
     watchFetchQuiz(),
     watchSignupUser(),
+    watchLoginUser(),
   ]);
 }
