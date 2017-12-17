b8a494f4bef475bb5046772a8bd6a1a73b68742d
Kamyl <kamillcybulski@gmail.com>
2017-12-18 00:34:13 +0100

    Add Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <nav className="primary-navbar">
+    <div>
+      <Link to="/" className="primary-navbar__logo">YouKnowJS</Link>
+    </div>
+    <div>
+      <Link to="/" className="primary-navbar__link">Home</Link>
+      <Link to="/quiz/12" className="primary-navbar__link">Quiz12</Link>
+      <Link to="/quiz/8" className="primary-navbar__link">Quiz8</Link>
+    </div>
+  </nav>
+);
+
+Nav.propTypes = {
+
+};
+
+export default Nav;
