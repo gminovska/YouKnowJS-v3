b9237a471ddd964247969649cdf9a9ea694f6d83
Kamyl <kamillcybulski@gmail.com>
2018-01-19 22:29:59 +0100

    Implement routing

--- src/containers/Routes.jsx
+++ src/containers/Routes.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Quiz from './Quiz';
+import Login from './Login';
+import Signup from './Signup';
 import Nav from '../components/Nav';
-
 
 const Routes = () => (
   <HashRouter>
     <div>
       <Nav />
-      <Route exact path="/" component={Home} />
-      <Route path="/quiz/:id" component={Quiz} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/user/login" component={Login} />
+        <Route path="/user/signup" component={Signup} />
+      </Switch>
     </div>
   </HashRouter>
 );
