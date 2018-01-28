df5241c475756ae9754befc73f40022ae152ab5b
Kamyl <kamillcybulski@gmail.com>
2018-01-28 22:38:09 +0100

    Add type prop for TextField

--- src/components/forms/TextField.jsx
+++ src/components/forms/TextField.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TextField = ({ input, meta: { touched, error } }) => (
+const TextField = ({ input, type, meta: { touched, error } }) => (
   <div>
-    <input type="text" {...input} />
+    <input type={type} {...input} />
     <div>
       {touched && error && <span>{error}</span>}
     </div>
   </div>
 );
+
+TextField.defaultProps = {
+  type: 'text',
+};
+
+/* eslint-disable react/forbid-prop-types */
+TextField.propTypes = {
+  input: PropTypes.object.isRequired,
+  meta: PropTypes.object.isRequired,
+  type: PropTypes.string,
+};
 
 export default TextField;
