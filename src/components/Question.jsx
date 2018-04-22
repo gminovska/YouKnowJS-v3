01d95f1db7a332822b9e53391ebe210033c0e254
Gordana Minovska <gordanam@fileonq.com>
2018-04-22 15:12:09 -0700

    Refactor no answer validation

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -10,27 +10,48 @@ import {
 } from '../actions/current-quiz';
 import AnswersList from './AnswersList';
 
-const Question = ({ question, submitSingle, submitMultiple }) => (
-  <div>
-    <p>{question.text}</p>
-    <AnswersList
-      answers={question.answers}
-      multipleChoice={question.multipleChoice}
-      onSubmit={question.multipleChoice ? submitMultiple : submitSingle}
-    />
-  </div>
-);
+class Question extends React.Component {
+  static propTypes = {
+    submitSingle: PropTypes.func.isRequired,
+    submitMultiple: PropTypes.func.isRequired,
+    question: PropTypes.shape({
+      answers: PropTypes.array,
+      explanation: PropTypes.string,
+      multipleChoice: PropTypes.bool,
+      text: PropTypes.string,
+    }).isRequired,
+  }
 
-Question.propTypes = {
-  submitSingle: PropTypes.func.isRequired,
-  submitMultiple: PropTypes.func.isRequired,
-  question: PropTypes.shape({
-    answers: PropTypes.array,
-    explanation: PropTypes.string,
-    multipleChoice: PropTypes.bool,
-    text: PropTypes.string,
-  }).isRequired,
-};
+  validateSingle = (values) => {
+    try {
+      this.props.submitSingle(values);
+    } catch (err) {
+      throw new SubmissionError({ answer: 'No answer error', _error: 'Please select an answer' });
+    }
+  }
+
+  validateMultiple = (values) => {
+    if (Object.keys(values).length !== 0) {
+      this.props.submitMultiple(values);
+    } else {
+      throw new SubmissionError({ answer: 'No answer error', _error: 'Please select an answer' });
+    }
+  }
+
+  render() {
+    const { question } = this.props;
+    return (
+      <div>
+        <p>{question.text}</p>
+        <AnswersList
+          answers={question.answers}
+          multipleChoice={question.multipleChoice}
+          onSubmit={question.multipleChoice ? this.validateMultiple : this.validateSingle}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   question: state.currentQuiz.questions[state.currentQuiz.currentIndex],
@@ -38,20 +59,12 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   submitSingle(values) {
-    try {
-      dispatch(submitSingleChoiceAnswer(values.answer));
-      dispatch(toggleExplanation());
-    } catch (err) {
-      throw new SubmissionError({ answer: 'No answer error', _error: 'Please select an answer' });
-    }
+    dispatch(submitSingleChoiceAnswer(values.answer));
+    dispatch(toggleExplanation());
   },
   submitMultiple(values) {
-    if (Object.keys(values).length !== 0) {
-      dispatch(submitMultipleChoiceAnswer(values));
-      dispatch(toggleExplanation());
-    } else {
-      throw new SubmissionError({ answer: 'No answer error', _error: 'Please select an answer' });
-    }
+    dispatch(submitMultipleChoiceAnswer(values));
+    dispatch(toggleExplanation());
   },
 });
 
