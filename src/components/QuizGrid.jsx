738560a3e29a8c47b51c0879afa5d7731013aafb
Gordana Minovska <gordanam@fileonq.com>
2018-05-04 14:52:26 -0700

    Style quiz grid

--- src/components/QuizGrid.jsx
+++ src/components/QuizGrid.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import { Grid } from 'semantic-ui-react';
 import QuizThumbnail from './QuizThumbnail';
 
 const QuizGrid = ({ quizzes }) => (
-  <div>
+  <Grid container stackable columns={3}>
     {quizzes.map(quiz => (
-      <QuizThumbnail
-        key={quiz.id}
-        id={quiz.id}
-        description={quiz.description}
-        name={quiz.name}
-      />
+      <Grid.Column key={quiz.id}>
+        <QuizThumbnail
+          id={quiz.id}
+          description={quiz.description}
+          name={quiz.name}
+          img={quiz.imgUrl}
+        />
+      </Grid.Column>
     ))}
-  </div>
+  </Grid>
 );
 
 QuizGrid.propTypes = {
--- src/components/QuizThumbnail.jsx
+++ src/components/QuizThumbnail.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Card, Button, Image } from 'semantic-ui-react';
 
-const QuizThumbnail = ({ description, name, id }) => (
-  <div>
-    Thumbnail
-    <Link to={`/quiz/${id}`}>
-      {name}
-    </Link>
-    {description}
-  </div>
+const QuizThumbnail = ({ description, name, id, img }) => (
+  <Card raized >
+    <Image style={{ height: '300px', objectFit: 'scale-down' }} size="medium" src={img} />
+    <Card.Content>
+      <Card.Header>
+        {name}
+      </Card.Header>
+      <Card.Description>
+        {description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Link to={`/quiz/${id}`}>
+        <Button inverted secondary>
+          Take quiz
+        </Button>
+      </Link>
+    </Card.Content>
+  </Card>
 );
 
 QuizThumbnail.propTypes = {
   description: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
 };
 
 export default QuizThumbnail;
